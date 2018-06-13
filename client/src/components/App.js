c6c6f88dbc47ac7ddc769803ebb9effd78040b91
Alex Bennett <benedictus.ab@gmail.com>
2018-06-13 13:17:33 -0600

    chatwindow/actions/reducers

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,6 +5,7 @@ import Login from './Login';
 import Register from './Register';
 import Flash from './Flash';
 import Home from './Home';
+import ChatWindow from './ChatWindow';
 import ProtectedRoute from './ProtectedRoute';
 import AuthRoute from './AuthRoute';
 import FetchUser from './FetchUser';
@@ -21,6 +22,7 @@ class App extends Component {
             <Route exact path='/' component={Home} />
             <AuthRoute exact path='/login' component={Login} />
             <AuthRoute exact path='/register' component={Register} />
+            <ProtectedRoute exact path='/chat' component={ChatWindow} />
             <Route component={NoMatch} />
           </Switch>
         </FetchUser>
